3f3eaa758cb8f7a506f4ea66934b6f643a1c19e6
Phạm Thành Trung <adamward459@gmail.com>
2024-01-28 16:40:07 +0700

    Fix hashing algorithm in authentication service

--- src/modules/authentication/service.ts
+++ src/modules/authentication/service.ts
@@ -3,12 +3,12 @@ import crypto from "node:crypto";
 import { createRevokedToken, findTokenByHash } from "../../db";
 
 export async function isTokenRevoked(prisma: PrismaClient, token: string) {
-  const hashedToken = crypto.createHash("sha216").update(token).digest("hex");
+  const hashedToken = crypto.createHash("sha256").update(token).digest("hex");
   const foundRevokedToken = await findTokenByHash(prisma, hashedToken);
   return Boolean(foundRevokedToken);
 }
 
 export async function revokeToken(prisma: PrismaClient, token: string) {
-  const hashedToken = crypto.createHash("sha216").update(token).digest("hex");
+  const hashedToken = crypto.createHash("sha256").update(token).digest("hex");
   await createRevokedToken(prisma, hashedToken);
 }
