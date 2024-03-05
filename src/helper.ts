4dc293468ca8b199d4fcd6e60ad1c35d6b08772f
Phạm Thành Trung <adamward459@gmail.com>
2024-03-05 23:57:09 +0700

    🔥 refactor: remove unused createFullName function

--- src/helper.ts
+++ src/helper.ts
@@ -4,14 +4,3 @@ export function createServerURL(path: string) {
   return encodeURIComponent(config.backend.domain + path);
 }
 
-export function createFullName(firstName?: string, lastName?: string): string {
-  if (firstName && lastName) {
-    return `${firstName} ${lastName}`;
-  } else if (firstName) {
-    return firstName;
-  } else if (lastName) {
-    return lastName;
-  } else {
-    return "";
-  }
-}
